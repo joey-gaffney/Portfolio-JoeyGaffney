32c41b345a07e08a810040e52c5ab2ee44770d8e
Joey Gaffney <joeygaffney22@gmail.com>
2022-07-30 17:44:11 -0700

    ui change

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// NAV MENU
+const hamburger = document.getElementById('hamburger')
+const navUL = document.getElementById('nav-ul')
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
+
 window.addEventListener('scroll', reveal);
 
 function reveal() {
