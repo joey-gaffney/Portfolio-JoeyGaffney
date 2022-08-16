b262193910983487b639b60b2831c21893ced1b0
Joey Gaffney <joeygaffney22@gmail.com>
2022-08-15 20:31:50 -0700

    style

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const navbar = document.getElementById('navbar');
-let scrolled = false;
+// const navbar = document.getElementById('navbar');
+// let scrolled = false;
 
-window.onscroll = function() {
-    if(window.pageYOffset > 100) {
-        navbar.classList.remove('top');
-        if(!scrolled) {
-            navbar.style.transform = 'translateY(-100px)';
-        }
-        setTimeout(function() {
-            navbar.style.transform = 'translateY(0)';
-            scrolled = true;
-        }, 200)
-    } else {
-        navbar.classList.add('top');
-        scrolled = false;
-    }
-}
+// window.onscroll = function() {
+//     if(window.pageYOffset > 100) {
+//         navbar.classList.remove('top');
+//         if(!scrolled) {
+//             navbar.style.transform = 'translateY(-100px)';
+//         }
+//         setTimeout(function() {
+//             navbar.style.transform = 'translateY(0)';
+//             scrolled = true;
+//         }, 200)
+//     } else {
+//         navbar.classList.add('top');
+//         scrolled = false;
+//     }
+// }
 
-$('#navbar a, .btn').on('click', function (e) {
-    if (this.hash !== '') {
-        e.preventDefault();
+// $('#navbar a, .btn').on('click', function (e) {
+//     if (this.hash !== '') {
+//         e.preventDefault();
 
-        const hash = this.hash;
+//         const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 100,
-            },
-            800
-        );
-    }
-});
+//         $('html, body').animate(
+//             {
+//                 scrollTop: $(hash).offset().top - 100,
+//             },
+//             800
+//         );
+//     }
+// });
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
 
 // window.addEventListener('scroll', reveal);
 
