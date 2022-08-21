be2c1eba19715d58e3f2ee26c84a0b40d784559c
Joey Gaffney <joeygaffney22@gmail.com>
2022-08-20 19:31:59 -0700

    toggle

--- app.js
+++ app.js
@@ -34,9 +34,11 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbarBar = document.getElementsByClassName('navbar')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
+    navbarBar.classList.toggle('active')
 })
 
 function reveal() {
